0d9c0673ccb4805d795805834c156bbe53b3dcf6
Vincent Tavernier <vince.tavernier@gmail.com>
2020-11-11 17:21:41 +0100

    Support non-SpirV preprocessing

--- build.rs
+++ build.rs
@@ -1,3 +1,28 @@
+macro_rules! compile_method {
+    ($frag:ident, $prog:ident, $path:literal, $name:literal, $prefer_spirv:ident, $wrapped:ident, $set:ident, $compiler:ident, $reflector:ident, $quad_vert:ident) => {
+        let sh = GlslObject::from_path($path, None)?.track_cargo();
+
+        if $prefer_spirv {
+            let sh = sh.compile(&mut $compiler)?.reflect_spirv(&$reflector)?;
+            $frag = $compiler.wrap_shader(sh, true).unwrap();
+        } else {
+            let sh = sh
+                .preprocess(&mut $compiler)?
+                .compile(&mut $compiler)?
+                .reflect_spirv(&$reflector)?;
+            $frag = $compiler.wrap_shader(sh, false).unwrap();
+        }
+
+        $prog = $compiler
+            .wrap_program(&[&$quad_vert, &$frag], $name)
+            .unwrap();
+
+        $wrapped.push(&$frag);
+        $wrapped.push(&$prog);
+        $set.push(&$prog);
+    };
+}
+
 #[cfg(all(feature = "gpu", feature = "wrap-shaders"))]
 fn wrap_shaders() -> anyhow::Result<()> {
     use std::env;
@@ -28,33 +53,33 @@ fn wrap_shaders() -> anyhow::Result<()> {
     wrapped.push(&quad_vert);
 
     if cfg!(feature = "method-debug") {
-        let sh = GlslObject::from_path("shaders/debug.frag", None)?
-            .track_cargo()
-            .compile(&mut compiler)?
-            .reflect_spirv(&reflector)?;
-        debug_frag = compiler.wrap_shader(sh, prefer_spirv).unwrap();
-        debug_prog = compiler
-            .wrap_program(&[&quad_vert, &debug_frag], "debug")
-            .unwrap();
-
-        wrapped.push(&debug_frag);
-        wrapped.push(&debug_prog);
-        set.push(&debug_prog);
+        compile_method!(
+            debug_frag,
+            debug_prog,
+            "shaders/debug.frag",
+            "debug",
+            prefer_spirv,
+            wrapped,
+            set,
+            compiler,
+            reflector,
+            quad_vert
+        );
     }
 
     if cfg!(feature = "method-whitenoise") {
-        let sh = GlslObject::from_path("shaders/whitenoise.frag", None)?
-            .track_cargo()
-            .compile(&mut compiler)?
-            .reflect_spirv(&reflector)?;
-        whitenoise_frag = compiler.wrap_shader(sh, prefer_spirv).unwrap();
-        whitenoise_prog = compiler
-            .wrap_program(&[&quad_vert, &whitenoise_frag], "whitenoise")
-            .unwrap();
-
-        wrapped.push(&whitenoise_frag);
-        wrapped.push(&whitenoise_prog);
-        set.push(&whitenoise_prog);
+        compile_method!(
+            whitenoise_frag,
+            whitenoise_prog,
+            "shaders/whitenoise.frag",
+            "whitenoise",
+            prefer_spirv,
+            wrapped,
+            set,
+            compiler,
+            reflector,
+            quad_vert
+        );
     }
 
     let global_set = compiler.wrap_uniforms(&set[..], "global").unwrap();
