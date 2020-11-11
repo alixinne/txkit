0973566f30106094bbd1cde77707265958964b5b
Vincent Tavernier <vince.tavernier@gmail.com>
2020-11-11 18:03:48 +0100

    Configure CI

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+//! [![Build Status](https://travis-ci.com/vtavernier/txkit.svg?branch=master)](https://travis-ci.com/vtavernier/txkit)
+//!
 //! TextureKit (txkit) is an implementation of common procedural texturing techniques used in
 //! computer graphics. It's a Rust library which can be used from other Rust programs as well as
 //! through its C API.
