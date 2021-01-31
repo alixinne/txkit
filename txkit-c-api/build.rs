45d83037a1f5b69eed8d8148cdd53bf349534a5e
Vincent Tavernier <vince.tavernier@gmail.com>
2021-01-31 23:35:53 +0100

    Fix txkit-c-api/build.rs on Windows

--- txkit-c-api/build.rs
+++ txkit-c-api/build.rs
@@ -1,18 +1,22 @@
+use quote::quote;
+
 fn main() {
+    let out_dir = std::env::var("OUT_DIR").unwrap();
+    let target = std::env::var("TARGET").unwrap();
+    let opt_level = std::env::var("OPT_LEVEL").unwrap();
+    let host = std::env::var("HOST").unwrap();
+
+    let module = quote! {
+        pub const OUT_DIR: &'static str = #out_dir;
+        pub const TARGET: &'static str = #target;
+        pub const OPT_LEVEL: &'static str = #opt_level;
+        pub const HOST: &'static str = #host;
+    };
+
     std::fs::write(
         std::path::PathBuf::from(std::env::var("OUT_DIR").expect("Failed to get OUT_DIR"))
             .join("config.rs"),
-        format!(
-            r#"pub const OUT_DIR: &'static str = "{}";
-    pub const TARGET: &'static str = "{}";
-    pub const OPT_LEVEL: &'static str = "{}";
-    pub const HOST: &'static str = "{}";
-    "#,
-            std::env::var("OUT_DIR").unwrap(),
-            std::env::var("TARGET").unwrap(),
-            std::env::var("OPT_LEVEL").unwrap(),
-            std::env::var("HOST").unwrap()
-        ),
+        module.to_string(),
     )
     .unwrap();
 }
